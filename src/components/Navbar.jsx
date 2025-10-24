9dd3ec456824058507b7d6704688e3e5e276b7f0
david <davmwa90@gmail.com>
2025-10-25 02:00:45 +0300

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,13 @@ import { CiMenuFries } from "react-icons/ci";
 import { RiCloseLargeLine } from "react-icons/ri";
 import {IoMdCall} from "react-icons/io"
 import { MdEmail } from "react-icons/md";
+import {
+  FaFacebookF,
+  FaXTwitter,
+  FaLinkedinIn,
+  FaWhatsapp,
+  FaPinterestP,
+} from "react-icons/fa6";
 
 
 
@@ -20,6 +27,40 @@ const [scroll, setScroll] = useState(false);
     { title: "FAQs", url: "faq" },
     { title: "Contact", url: "contact" },
   ];
+  const socials = [
+    {
+      icon: <FaFacebookF />,
+      url: "https://www.facebook.com/p/Big-Brothers-Enterprise-100089368881036/",
+
+      bgColor: "bg-blue-600",
+      color: "text-white",
+    },
+    {
+      icon: <FaXTwitter />,
+
+      bgColor: "bg-sky-500",
+      color: "text-white",
+    },
+    {
+      icon: <FaLinkedinIn />,
+
+      bgColor: "bg-[#0077B5]",
+      color: "text-white",
+    },
+    // {
+    //   icon: <FaWhatsapp />,
+
+    //   bgColor: "bg-[#00b54b]",
+    //   color: "text-white",
+    // },
+    {
+      icon: <FaPinterestP />,
+
+      bgColor: "bg-[#e21d1d]",
+      color: "text-white",
+    },
+  ];
+
 
   const toggleMobileMenu = () => {
     setShowMenu(prev => !prev)
@@ -51,6 +92,7 @@ useEffect(() => {
       } fixed top-0`}
     >
       {/* desktop */}
+
       <div className="w-full h-full items-center max-w-[1200px] mx-auto hidden md:flex justify-between px-2 md:px-0">
         {/* logo */}
         <div className="w-[250px] cursor-pointer">
@@ -89,14 +131,30 @@ useEffect(() => {
             ))}
           </div>
         </div>
+
         <div>
-          <a
-            href="https://wa.me/254716903314?text=Thank%20you%20for%20contacting%20Big%20Brothers%20Enterprise"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="btnOne">WhatsApp</button>
-          </a>
+          <div className="flex justify-center gap-2">
+            {socials.map((social, index) => (
+              <a
+                key={index}
+                href={social.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`hover:scale-125 duration-700 p-2 rounded-full ${social.bgColor} ${social.color}`}
+              >
+                {social.icon}
+              </a>
+            ))}
+          </div>
+          <div className="mt-4 flex justify-center">
+            <a
+              href="https://wa.me/254716903314?text=Thank%20you%20for%20contacting%20Big%20Brothers%20Enterprise"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="btnOne">WhatsApp</button>
+            </a>
+          </div>
         </div>
       </div>
 
@@ -143,7 +201,7 @@ useEffect(() => {
 
         {/* Mobile Menu */}
         <div
-          className={`fixed Z-50 top-0 w-[50%] left-0 bg-(--secondary) h-screen md:hidden transition-transform duration-700 ${
+          className={`fixed top-0 w-[50%] left-0 bg-(--secondary) h-screen md:hidden transition-transform duration-700  ${
             showMenu ? "translate-x-0" : "-translate-x-full"
           }`}
         >
@@ -176,14 +234,31 @@ useEffect(() => {
               ))}
             </div>
           </div>
-          <div className="pl-2">
-            <a
-              href="https://wa.me/254716903314?text=Thank%20you%20for%20contacting%20Big%20Brothers%20Enterprise"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className="btnOne">WhatsApp</button>
-            </a>
+          <div className="">
+            <div>
+              <div className="flex justify-center gap-1">
+                {socials.map((social, index) => (
+                  <a
+                    key={index}
+                    href={social.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={`hover:scale-125 duration-700 p-2 rounded-full ${social.bgColor} ${social.color}`}
+                  >
+                    {social.icon}
+                  </a>
+                ))}
+              </div>
+              <div className="mt-4 flex justify-center">
+                <a
+                  href="https://wa.me/254716903314?text=Thank%20you%20for%20contacting%20Big%20Brothers%20Enterprise"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="btnOne">WhatsApp</button>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
