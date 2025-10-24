f74e16790d449064735831752e70bf6bb46db24b
david <davmwa90@gmail.com>
2025-10-24 23:45:51 +0300

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const [showMenu, setShowMenu] = useState(false)
     setShowMenu(prev => !prev)
   }
   return (
-    <div className="w-full h-22 md:bg-gray-300 sticky top-0">
+    <div className="w-full h-16 md:h-22 bg-(--secondary) fixed top-0">
       {/* desktop */}
       <div className="w-full h-full items-center max-w-[1200px] mx-auto hidden md:flex justify-between px-2 md:px-0">
         {/* logo */}
@@ -37,31 +37,53 @@ const [showMenu, setShowMenu] = useState(false)
                 className="cursor-pointer"
                 smooth={true}
                 spy={true}
+                activeClass="text-(--primaryOne)"
               >
                 {link.title}
               </Link>
             </div>
           ))}
         </div>
+        <div>
+          <a
+            href="https://wa.me/254716903314?text=Thank%20you%20for%20contacting%20Big%20Brothers%20Enterprise"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="btnOne">WhatsApp</button>
+          </a>
+        </div>
       </div>
 
       {/* mobile */}
       <div className="fixed md:hidden z-60 top-0 left-0 w-full">
         {/* Toggle Button */}
-        <div
-          className="absolute right-4 top-4 text-black"
-          onClick={toggleMobileMenu}
-        >
-          {showMenu ? (
-            <RiCloseLargeLine size={20} />
-          ) : (
-            <CiMenuFries size={30} />
-          )}
+        <div>
+          <div className="px-2">
+            <Link onClick={toggleMobileMenu} to={"banner"}>
+              <img
+                src="./images/big_brother.png"
+                alt="Big Brother Logo"
+                className="w-[100px]"
+              />
+            </Link>
+          </div>
+
+          <div
+            className="absolute right-4 top-4 text-black"
+            onClick={toggleMobileMenu}
+          >
+            {showMenu ? (
+              <RiCloseLargeLine size={20} />
+            ) : (
+              <CiMenuFries size={30} />
+            )}
+          </div>
         </div>
 
         {/* Mobile Menu */}
         <div
-          className={`fixed top-0 w-[50%] left-0 bg-gray-200 h-screen md:hidden transition-transform duration-700 ${
+          className={`fixed top-0 w-[50%] left-0 bg-(--secondary) h-screen md:hidden transition-transform duration-700 ${
             showMenu ? "translate-x-0" : "-translate-x-full"
           }`}
         >
@@ -81,11 +103,27 @@ const [showMenu, setShowMenu] = useState(false)
             {/* Links */}
             <div className="flex flex-col gap-2 p-4">
               {links.map((mobile_link, index) => (
-                <Link onClick={toggleMobileMenu} key={index} to={mobile_link.url}>
+                <Link
+                  onClick={toggleMobileMenu}
+                  key={index}
+                  to={mobile_link.url}
+                  smooth={true}
+                  spy={true}
+                  activeClass="text-(--primaryOne)"
+                >
                   {mobile_link.title}
                 </Link>
               ))}
             </div>
+          </div>
+          <div className="pl-2">
+            <a
+              href="https://wa.me/254716903314?text=Thank%20you%20for%20contacting%20Big%20Brothers%20Enterprise"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="btnOne">WhatsApp</button>
+            </a>
           </div>
         </div>
       </div>
