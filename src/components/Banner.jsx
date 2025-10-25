825a15f48f657c9ca0b72234a753769c4f095324
david <davmwa90@gmail.com>
2025-10-25 06:59:07 +0300

    created a new app

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -16,10 +16,10 @@ const Banner = () => {
         <div className="top-0 absolute flex flex-col pt-60 md:pt-0 items-center md:justify-center left-0 h-full w-full z-0">
           <div className="flex flex-col px-2">
             <div className="flex flex-col items-center bg-gray-900/60 px-2 md:px-8 py-4 rounded-2xl">
-              <h1 className="text-3xl lg:text-5xl font-bold text-(--secondary)">
-                Big Brother Enterprises
+              <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold text-(--secondary)">
+                Big Brothers Enterprise
               </h1>
-              <p className="px-4 text-center text-2xl lg:text-4xl text-orange-400">
+              <p className="px-4 text-center text-xl md:text-2xl lg:text-4xl text-orange-400">
                 Dealers in construction of memorial tombstones,
                 headstones,(Terrazzo & Granite), plaques & Pulpits
               </p>
