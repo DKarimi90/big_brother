1b65ee9229a7271ee14bd69057ca1fb4cadf8c3a
david <davmwa90@gmail.com>
2025-10-25 08:17:16 +0300

    up

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -54,7 +54,7 @@ const Services = () => {
     <div name="services" className="w-full pt-10">
       <div className="">
         <div className="w-full flex flex-col items-center">
-          <h1 className="font-bold text-2xl md:text-4xl lg:text-6xl border-b mb-2">
+          <h1 className="font-bold text-2xl text-(--primaryOne) md:text-4xl lg:text-6xl border-b mb-2">
             Our <span className="text-(--primary)">Services</span>
           </h1>
           <p className="text-center">
